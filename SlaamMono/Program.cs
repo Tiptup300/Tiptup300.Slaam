ded183c34fc2ff6d0df9b7d160a01c23d27be661
tiptup300 <tiptup300@gmail.com>
2021-09-23 16:44:39 -0400

    Cleaned up Program Entry

--- SlaamMono/Program.cs
+++ SlaamMono/Program.cs
@@ -4,66 +4,43 @@ namespace SlaamMono
 {
     static class Program
     {
-        #region Version
-
         /// <summary>
         /// Current version of Slaam! Running (for online purposes)
         /// </summary>
-        public static byte[] Version = new byte[]
-        {
-                000,
-                000,
-                000,
-                002,
-        };
-
-        #endregion
-
-        #region Entry Point
+        public static byte[] Version = new byte[] { 000, 000, 000, 002,};
 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main(string[] args)
         {
-            LogHelper.Begin();
-            LogHelper.Write("=======================================");
-            LogHelper.Write("Slaam! - Logfile (for errors)");
-            LogHelper.Write(" Created by Tiptup300");
-            LogHelper.Write("=======================================");
-            LogHelper.Write("");
+            LogHelper.Instance.Begin();
+
             LogHelper.Write("Program executed;");
             LogHelper.Write("XNA Starting...");
-#if !DEBUG
-            //try
-            //{
-#endif
-                using (SlaamGame game = new SlaamGame())
-                {
-                    game.Run();
-                }
-#if !DEBUG
-            /*}
+
+            try
+            {
+            using (SlaamGame game = new SlaamGame())
+            {
+                game.Run();
+            }
+
+            }
             catch (Exception e)
             {
                 LogHelper.Write("Game Failed With Exit Message: ");
-                LogHelper.Write(e.Message,false);
+                LogHelper.Write(e.Message);
                 if (e.InnerException != null)
                 {
-                    LogHelper.Write(" --- INNER Exception --- ", false);
-                    LogHelper.Write(e.InnerException.ToString(), false);
+                    LogHelper.Write(" --- INNER Exception --- ");
+                    LogHelper.Write(e.InnerException.ToString());
                 }
-                LogHelper.Write(" --- STACK TRACE --- ", false);
-                LogHelper.Write(e.StackTrace, false);
+                LogHelper.Write(" --- STACK TRACE --- ");
+                LogHelper.Write(e.StackTrace);
                 LogHelper.End();
-                MessageBox.Show("An error occured, please report this problem, including your log.log \n" +
-                                "MSG: \"" + e.Message + "\""
-                    , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-            }*/
-#endif
             }
-
-        #endregion
+        }
     }
 }
 
