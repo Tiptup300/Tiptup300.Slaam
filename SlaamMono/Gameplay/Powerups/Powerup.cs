8a16dbc65fd6b018d8524fdccf30d0d32b6dfa68
tiptup300 <tiptup300@gmail.com>
2022-02-23 15:41:57 -0500

    code cleanup

--- SlaamMono/Gameplay/Powerups/Powerup.cs
+++ SlaamMono/Gameplay/Powerups/Powerup.cs
@@ -1,7 +1,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SlaamMono.Gameplay.Actors;
-using SlaamMono.Gameplay.Boards;
 using SlaamMono.Library.ResourceManagement;
 
 namespace SlaamMono.Gameplay.Powerups
